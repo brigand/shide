99b878fce39086cc1f8a813b1438d591449a2653
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-11-25 20:55:30 -0700

    initial commit

---
+++ atom-shide/lib/atom-shide.js
@@ -0,0 +1,48 @@
+'use babel';
+
+import AtomShideView from './atom-shide-view';
+import { CompositeDisposable } from 'atom';
+
+export default {
+
+  atomShideView: null,
+  modalPanel: null,
+  subscriptions: null,
+
+  activate(state) {
+    this.atomShideView = new AtomShideView(state.atomShideViewState);
+    this.modalPanel = atom.workspace.addModalPanel({
+      item: this.atomShideView.getElement(),
+      visible: false
+    });
+
+    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+    this.subscriptions = new CompositeDisposable();
+
+    // Register command that toggles this view
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'atom-shide:toggle': () => this.toggle()
+    }));
+  },
+
+  deactivate() {
+    this.modalPanel.destroy();
+    this.subscriptions.dispose();
+    this.atomShideView.destroy();
+  },
+
+  serialize() {
+    return {
+      atomShideViewState: this.atomShideView.serialize()
+    };
+  },
+
+  toggle() {
+    return (
+      this.modalPanel.isVisible() ?
+      this.modalPanel.hide() :
+      this.modalPanel.show()
+    );
+  }
+
+};
