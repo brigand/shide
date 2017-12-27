260e6f28c0f8534927c5ba815173c018bc21b0e7
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-12-26 22:17:39 -0500

    'select' scroll into view (mostly working)

--- atom-shide/lib/ui/SelectOptions.js
+++ atom-shide/lib/ui/SelectOptions.js
@@ -11,10 +11,41 @@ class SelectOptions extends Component {
 
   render() {
     return (
-      <div class="ShideSelectOptions">
+      <div class="ShideSelectOptions" ref={(el) => { this.el = el; }}>
         {this.props.options.map((x, i) => this.renderItem(x, i))}
       </div>
     );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.activeItem) this.scrollIntoView();
+  }
+
+  scrollIntoView() {
+    const active = this.el.querySelector('.ShideSelectOptions__Item--active');
+    if (!active) return;
+
+    // Find the scroll container
+    let scrollCont = active.parentNode;
+    while (scrollCont && scrollCont.clientHeight >= scrollCont.scrollHeight) {
+      scrollCont = scrollCont.parentNode;
+    }
+
+    if (!scrollCont) return;
+    if (scrollCont.tagName !== 'DIV') return;
+
+    const ib = active.getBoundingClientRect();
+    const cb = scrollCont.getBoundingClientRect();
+    let ctop = scrollCont.scrollTop;
+
+    if (ib.top < cb.top) {
+      ctop += ib.top - cb.top - ib.height;
+    }
+    else if (ib.bottom > cb.bottom) {
+      ctop += ib.bottom - cb.bottom;
+    }
+
+    scrollCont.scrollTop = ctop;
   }
 
   renderItem(x, i) {
